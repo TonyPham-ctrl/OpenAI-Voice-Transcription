d5c472723df0671fcb6bdce8d1276db624cea51b
Tony Pham <tonyadpham@gmail.com>
2026-09-10 20:46:54 +0930

    setup api cals and response on frontend side

--- assignment1/src/main/resources/static/app.js
+++ assignment1/src/main/resources/static/app.js
@@ -13,29 +13,51 @@ const serverResponseForm = document.getElementById('serverOutput');
 
 
 requestStatsBtn.addEventListener('click', async () => {
-
     serverResponseForm.textContent = 'Fetching stats...';
+    const res = await fetch('/api/stats', {
+        method: 'GET'
+    });
 
+    const data = await res.json();
+    serverResponseForm.textContent = `Stats: ${JSON.stringify(data)}`;
 });
 
 
 requestUptimeBtn.addEventListener('click', async () => {
-
     serverResponseForm.textContent = 'Fetching uptime...';
+    const res = await fetch('/api/uptime', {
+        method: 'GET'
+    });
 
+    const data = await res.json();
+    serverResponseForm.textContent = `Uptime: ${data.uptime}`;
 });
 
 requestShutdown.addEventListener('click', async () => {
 
     serverResponseForm.textContent = 'Sending shutdown request...';
 
+    const res = await fetch('/api/shutdown', {
+        method: 'POST'
+    });
+
+    const data = await res.json();
+    serverResponseForm.textContent = `Shutdown request sent: ${data.message}`;
 });
 
 clearResponseBtn.addEventListener('click', async () => {
-    
     serverResponseForm.textContent = '';
 });
 
 uploadVoiceBtn.addEventListener('click', async () => {
     serverResponseForm.textContent = 'Uploading voice file...';
+    const formData = new FormData();
+    formData.append('client_voice_input', voiceFile.files[0]);
+
+    const res = await fetch('/api/transcribe', {
+        method: 'POST',
+        body: formData
+    });
+    const data = await res.json();
+    serverResponseForm.textContent = `Transcription: ${data.transcription}`;
 });
