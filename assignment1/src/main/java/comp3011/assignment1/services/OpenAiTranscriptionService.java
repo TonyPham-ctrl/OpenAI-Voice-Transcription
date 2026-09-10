26f0167097c3e4c663ac440aae3240c2792b038a
Tony Pham <tonyadpham@gmail.com>
2026-09-11 02:31:35 +0930

    properly ignoring .env this time

--- assignment1/src/main/java/comp3011/assignment1/services/OpenAiTranscriptionService.java
+++ assignment1/src/main/java/comp3011/assignment1/services/OpenAiTranscriptionService.java
@@ -1,6 +1,7 @@
 package comp3011.assignment1.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.boot.jackson.autoconfigure.JacksonProperties.Json;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,5 +12,14 @@ public class OpenAiTranscriptionService implements TranscriptionService {
     public String transcribe(MultipartFile audioFile) {
         
         return "real transcription";
+  
+  
+  
+  
+  public JsonObject buildJsonRequest(MultipartFile audioFile) {
+        JsonObject jsonRequest = new JsonObject();
+        jsonRequest.addProperty("model", "whisper-1");
+        jsonRequest.addProperty("file", audioFile.getOriginalFilename());
+        return jsonRequest;
     }
 }
