377ba20a8c91e0b81b891b5ba37ca295478958b1
Tony Pham <tonyadpham@gmail.com>
2026-09-11 04:05:14 +0930

    added token tracking

--- assignment1/src/main/java/comp3011/assignment1/controllers/GlobalController.java
+++ assignment1/src/main/java/comp3011/assignment1/controllers/GlobalController.java
@@ -1,12 +1,27 @@
 package comp3011.assignment1.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
-@RequestMapping ("/api/v1/global")
+import comp3011.assignment1.models.GlobalStatsResponse;
+import comp3011.assignment1.models.ServerState;
+
+@RestController
+@RequestMapping("/api/v1/global")
 public class GlobalController {
-    public GlobalController() {
-        
-    };
+
+    private final ServerState serverState;
+
+    public GlobalController(ServerState serverState) {
+        this.serverState = serverState;
+    }
+
+    @GetMapping("/stats")
+    public GlobalStatsResponse getStats() {
+        return new GlobalStatsResponse(
+            serverState.getInputTokens(),
+            serverState.getOutputTokens()
+        );
+    }
 }
---
+++ assignment1/src/main/java/comp3011/assignment1/models/GlobalStatsResponse.java
@@ -0,0 +1,8 @@
+package comp3011.assignment1.models;
+
+
+
+public record GlobalStatsResponse (
+    long inputTokens,
+    long outputTokens
+) {}
