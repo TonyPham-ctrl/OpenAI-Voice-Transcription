14dbdb9af647c42d4607d9ed978efb067e4d3ecb
Tony Pham <tonyadpham@gmail.com>
2026-09-10 20:21:40 +0930

    more boiler plate codes for buttons

--- assignment1/src/main/resources/static/app.js
+++ assignment1/src/main/resources/static/app.js
@@ -1,9 +1,15 @@
 const connectionStatus = document.getElementById('connectionStatus');
 const voiceFile = document.getElementById('voiceFile');
+
 const requestStatsBtn = document.getElementById('statsButton');
 const requestUptimeBtn = document.getElementById('uptimeButton');
 const requestShutdown = document.getElementById('shutdownButton');
+const clearResponseBtn = document.getElementById('clearOutputButton');
+const uploadVoiceBtn = document.getElementById('uploadButton');
+
 const serverResponseForm = document.getElementById('serverOutput');
+
+
 
 
 requestStatsBtn.addEventListener('click', async () => {
@@ -11,7 +17,6 @@ requestStatsBtn.addEventListener('click', async () => {
     serverResponseForm.textContent = 'Fetching stats...';
 
 });
-
 
 
 requestUptimeBtn.addEventListener('click', async () => {
@@ -24,4 +29,13 @@ requestShutdown.addEventListener('click', async () => {
 
     serverResponseForm.textContent = 'Sending shutdown request...';
 
+});
+
+clearResponseBtn.addEventListener('click', async () => {
+    
+    serverResponseForm.textContent = '';
+});
+
+uploadVoiceBtn.addEventListener('click', async () => {
+    serverResponseForm.textContent = 'Uploading voice file...';
 });
